62ff2a4133242522265d230f99d431590acf4f5b
coder <coder_shadow@163.com>
2016-08-29 22:18:13 +0800

    参考资料1

---
+++ info.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<table>
+		<tr>
+			<td><a href="http://www.cnblogs.com/txw1958/p/weixin71-oauth20.html">授权登陆</a></td>
+		</tr>
+		<tr>
+			<td><a href="http://jingyan.baidu.com/article/60ccbceb07ec4764cbb19768.html">获取openid</a></td>
+		</tr>
+		<tr>
+			<td><a href="http://mp.weixin.qq.com/wiki/14/bb5031008f1494a59c6f71fa0f319c66.html">获取信息</a></td>
+		</tr>
+		<tr>
+			<td><a href="http://www.loveqiao.com/dom/html5css3/hecheng/">合并图片</a></td>
+		</tr>
+	</table>
+</body>
+</html>
